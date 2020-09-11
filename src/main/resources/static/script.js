0f361241274a45078cade7656488da5a0c69eda2
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-09-12 00:33:13 +0200

    Finished program

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,65 @@
+var firstNumber = 0;
+var secondNumber = 0;
+var operation;
+var operatorPressed = false;
+
+$(document).ready(function () {
+    $(".number").click( number );
+    $(".zero").click( number );
+    $(".operator").click( operator );
+    $(".delete").click( clear );
+    $(".equal").click( equals );
+});
+
+function number(){
+    var total = $(".total");
+    if(total.val() === 0 || !operatorPressed){
+        firstNumber += $(this).text();
+        //WHY DOES +firstNumber REMOVE the first letter !?!?!?!?!?!?!?
+        total.val(+firstNumber);
+        alert(firstNumber);
+    }
+    else{
+        secondNumber += $(this).text();
+        total.val(+secondNumber);
+    }
+}
+
+function operator(){
+    operation = $(this).text();
+    operatorPressed = true;
+    $(".total").val(operation);
+}
+
+function clear(){
+    $("#total").val(0);
+    firstNumber = 0;
+    secondNumber = 0;
+}
+
+function equals(){
+    var total;
+    //we parse our numbers into doubles
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    switch (operation) {
+        case "+":
+            total = firstNumber + secondNumber;
+            break;
+        case "-":
+            total = firstNumber - secondNumber;
+            break;
+        case "x":
+            total = firstNumber * secondNumber;
+            break;
+        case "/":
+            total = firstNumber / secondNumber;
+            break;
+        default:
+            alert("SOMETHING WENT TERRIBLY WRONG!");
+    }
+    $(".total").val(total);
+    firstNumber = total; //måske reset til nul, men på denne måde kan du lægge mere til resultatet
+    secondNumber = 0;
+    operatorPressed = false;
+}
